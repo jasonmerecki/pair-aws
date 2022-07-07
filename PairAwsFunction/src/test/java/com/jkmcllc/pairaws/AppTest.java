f4dd8bcacfa6b5e03a48e4d75d068a3e280be631
Jason Merecki <jason.merecki@maxar.com>
2022-07-06 21:24:09 -0500

    added samples

--- PairAwsFunction/src/test/java/com/jkmcllc/pairaws/AppTest.java
+++ PairAwsFunction/src/test/java/com/jkmcllc/pairaws/AppTest.java
@@ -80,6 +80,73 @@ public class AppTest {
   }
 
   @Test
+  public void successfulResponseCovered() {
+    App app = new App();
+    String bodyString = "{\n" +
+            "  \"AccountNumber\": \"JKM0001\",\n" +
+            "  \"AccountBalance\": {\n" +
+            "    \"TotalEquity\": \"0.00\",\n" +
+            "    \"CashTradeBalance\": \"0.00\",\n" +
+            "    \"MarginTradeBalance\": \"30921.39\",\n" +
+            "    \"ShortTradeBalance\": \"0.00\",\n" +
+            "    \"MoneyMarketTradeBalance\": \"0.00\",\n" +
+            "    \"SMA\": \"0.00\"\n" +
+            "  },\n" +
+            "  \"Positions\": [\n" +
+            "    {\n" +
+            "      \"AccountType\": \"String!\",\n" +
+            "      \"Symbol\": \"MSFT\",\n" +
+            "      \"SettleQuantity\": \"200\",\n" +
+            "      \"SecurityTypeCode\": \"S\",\n" +
+            "      \"ClosingPrice\": \"406.88\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"AccountType\": \"String!\",\n" +
+            "      \"Symbol\": \"MSFT  200515P00400000\",\n" +
+            "      \"SettleQuantity\": \"-7\",\n" +
+            "      \"SecurityTypeCode\": \"O\",\n" +
+            "      \"ClosingPrice\": \"0.01\",\n" +
+            "      \"OptionSymbolRoot\": \"SPY\",\n" +
+            "      \"OptionContractDate\": \"2020-05-15\",\n" +
+            "      \"StrikePrice\": \"400.000\",\n" +
+            "      \"CallPut\": \"P\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"OptionRoots\": [\n" +
+            "    {\n" +
+            "      \"OptionRootSymbol\": \"MSFT\",\n" +
+            "      \"ExerciseStyle\": \"A\",\n" +
+            "      \"UnderlyerType\": \"S\",\n" +
+            "      \"Multiplier\": \"100\",\n" +
+            "      \"Deliverables\": {\n" +
+            "        \"SymbolDeliverables\": [\n" +
+            "          {\n" +
+            "            \"symbol\": \"MSFT\",\n" +
+            "            \"qty\": \"100\",\n" +
+            "            \"price\": \"406.59\",\n" +
+            "            \"maintenancePct\": \"0.25\"\n" +
+            "          }\n" +
+            "        ],\n" +
+            "        \"CashDeliverableValue\": \"0\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+    APIGatewayProxyRequestEvent proxyEvent = new APIGatewayProxyRequestEvent();
+    proxyEvent.setBody(bodyString);
+    proxyEvent.setHttpMethod("POST");
+    APIGatewayProxyResponseEvent result = app.handleRequest(proxyEvent, null);
+    assertEquals(result.getStatusCode().intValue(), 200);
+    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+    String content = result.getBody();
+    assertNotNull(content);
+    // assertTrue(content.contains("\"message\""));
+    // assertTrue(content.contains("\"hello "));
+    // assertTrue(content.contains("\"location\""));
+    assertTrue(content.contains("SPY"));
+  }
+
+  @Test
   public void longResponse() {
     App app = new App();
     String bodyString = "{\n" +
